e4b0865a715ed11be7f8d5c911f5e29632a81ebb
Shivam Rajput <raj.shiviraj@gmail.com>
2024-12-06 08:50:59 +0530

    Shivi | Add logging interceptor in request.

--- src/webclient/interceptor.ts
+++ src/webclient/interceptor.ts
@@ -1,7 +1,13 @@
 import type {AxiosInstance, AxiosInterceptorOptions, AxiosResponse, InternalAxiosRequestConfig} from 'axios'
 import axios from 'axios'
+import logging from 'logging-starter'
 
 const api: AxiosInstance = axios.create()
+
+api.interceptors.request.use((config) => {
+  logging.info({message: 'Successfully sent api request', data: {method: config.method, url: config.url}})
+  return config
+})
 
 const interceptor = {
   request(
